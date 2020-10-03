836adee0eaa78aa781da2896d95c07d4fa839ca9
Vyacheslav Miroshko <vyacheslavm@wix.com>
2020-10-03 17:31:09 +0300

    Updates

---
+++ precache-manifest.6c2269e434158bf22b37146ad40917f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6de398925d5b3b97c0f7c3ae324afd90",
+    "url": "/colors-test-react/index.html"
+  },
+  {
+    "revision": "90aaca5a8d3146f05f74",
+    "url": "/colors-test-react/static/css/main.e612fc52.chunk.css"
+  },
+  {
+    "revision": "cef6a29f3ac50545deda",
+    "url": "/colors-test-react/static/js/2.b001e514.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/colors-test-react/static/js/2.b001e514.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "90aaca5a8d3146f05f74",
+    "url": "/colors-test-react/static/js/main.7716a5af.chunk.js"
+  },
+  {
+    "revision": "63449b4639925eb254b2",
+    "url": "/colors-test-react/static/js/runtime-main.e7fab4ee.js"
+  }
+]);
